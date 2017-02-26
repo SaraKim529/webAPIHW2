88769effe3c25b75924554f37ca674b03b4a1215
SaraKim529 <sara.kim@ucdenver.edu>
2017-02-26 03:56:34 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+// Require http and express application
+var http = require('http');
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// Use app.all to match HTTP requests
+
+    // if else checks that /gets only accepts GET request
+    app.all('/gets', function(request, response) {
+        if(request.method == 'GET') {
+            if(Object.keys(request.query).length !== 0) {
+                // req.query GET query parameters sent in
+                response.send(request.query);
+            }
+            else {
+                response.send('GET request accepted; No query parameters sent in');
+            }
+        }
+        else {
+            response.send('Request rejected! Request does not support HTTP method');
+        }
+    });
+
+    // if else checks that /posts only accepts POST request
+    app.all('/posts', function(request,response) {
+        if(request.method == 'POST') {
+            if(Object.keys(request.query).length !== 0) {
+                response.send(request.query);
+            }
+            else {
+                response.send('POST request accepted; No query parameters sent in');
+            }
+        }
+        else {
+            response.send('Request rejected! Request does not support HTTP method');
+        }
+    });
+
+    // if else checks that /puts only accepts PUT request
+    app.all('/puts', function(request,response) {
+        if(request.method == 'PUT') {
+            if(Object.keys(request.query).length !== 0) {
+                response.send(request.query);
+            }
+            else {
+                response.send('PUT request accepted; No query parameters sent in');
+            }
+        }
+        else {
+            response.send('Request rejected! Request does not support HTTP method');
+        }
+    });
+
+    // if else checks that /deletes only accepts DELETE request
+    app.all('/deletes', function(request,response) {
+        if(request.method == 'DELETE') {
+            if(Object.keys(request.query).length !== 0) {
+                response.send(request.query);
+            }
+            else {
+                response.send('DELETE request accepted; No query parameters sent in');
+            }
+        }
+        else {
+            response.send('Request rejected! Request does not support HTTP method');
+        }
+    });
+
+app.listen(3333);
+console.log('Go to http://localhost:3333');
