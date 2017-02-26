78f551c38610782433ec30a520373b146a8491e1
SaraKim529 <sara.kim@ucdenver.edu>
2017-02-26 04:00:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ var path = require('path');
     // if else checks that /gets only accepts GET request
     app.all('/gets', function(request, response) {
         if(request.method == 'GET') {
+            // checks to see if query parameter is empty or not
+            // by checking the request query's length
             if(Object.keys(request.query).length !== 0) {
                 // req.query GET query parameters sent in
                 response.send(request.query);
             }
+            // if query parameter is empty, state that GET was accepted but there were no query parameters
             else {
                 response.send('GET request accepted; No query parameters sent in');
             }
@@ -22,6 +25,7 @@ var path = require('path');
         }
     });
 
+// Everything below is similar to the above for GET, except it is for POST, PUT, and DELETE
     // if else checks that /posts only accepts POST request
     app.all('/posts', function(request,response) {
         if(request.method == 'POST') {
@@ -67,5 +71,6 @@ var path = require('path');
         }
     });
 
+// Can go to port localhost:3333 but app.js server is also hosted on apigee
 app.listen(3333);
 console.log('Go to http://localhost:3333');
